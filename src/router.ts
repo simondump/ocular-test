5389664e5454f78306e4dddc3e53d2b09423371e
simondump <42521610+simondump@users.noreply.github.com>
2025-11-02 14:48:55 +0100

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 export const router = createRouter({
-  history: createWebHistory('./'),
+  history: createWebHistory('/ocular/demo'),
   routes: [
     {
       path: '/',
@@ -20,7 +20,7 @@ export const router = createRouter({
     },
     {
       path: '/:pathMatch(.*)*',
-      redirect: './'
+      redirect: '/'
     }
   ]
 });
