813e248724144955cc5f3d3f823eac4c5f1fdc84
simondump <42521610+simondump@users.noreply.github.com>
2025-11-02 14:52:58 +0100

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 export const router = createRouter({
-  history: createWebHistory('/ocular/demo'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
